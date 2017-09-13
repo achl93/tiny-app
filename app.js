8d9eda719f3b0464b253b17a6bf114224aff8218
Andy Leung <andy_ch_009@hotmail.com>
2017-09-13 23:26:25 +0000

    add user specific features

--- app.js
+++ app.js
@@ -10,9 +10,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+  "b2xVn2": {
+    long: "http://www.lighthouselabs.ca",
+    id: "userRandomID"
+  },
+  "9sm5xK": {
+    long: "http://www.google.com",
+    id: "user2RandomID"
+  }
+}
 
 var users = { 
   "userRandomID": {
@@ -36,29 +42,33 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
+  const id = req.cookies.user_id;
   let templateVars = {
-    urls: urlDatabase
+    urls: urlsForUser(id)
   };
-  // console.log(req.cookies);
   res.render("urls_index", templateVars);
 })
 
 app.get("/urls/new", (req, res) => {
-  // let templateVars = {
-  //   user: users
-  // };
-  res.render("urls_new");
+  if (app.locals.user === undefined) {
+    res.redirect("/register");
+  }
+  else {
+    res.render("urls_new");
+  }
 });
 
 app.post("/urls", (req, res) => {
   let randomString = generateRandomString();
-  urlDatabase[randomString] = req.body.longURL;
+  const currCookie = req.cookies.user_id;
+  urlDatabase[randomString] = {};
+  urlDatabase[randomString].long = req.body.longURL;
+  urlDatabase[randomString].id = currCookie;
   res.redirect('http://localhost:8080/urls/'+randomString);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
-  // console.log(longURL);
+  let longURL = urlDatabase[req.params.shortURL].long;
   res.redirect(longURL);
 });
 
@@ -70,35 +80,43 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  res.redirect("/urls");
+  const currCookie = req.cookies.user_id;
+  if (currCookie === urlDatabase[req.params.id].id) {
+    delete urlDatabase[req.params.id];
+    res.redirect("/urls");
+  }
+  else {
+    res.status(403).send("403: This link does not belong to you ಠ_ಠ");
+  }
 })
 
 app.post("/urls/:id/update", (req, res) => {
   const re = /http:\/\//;
-  if (req.body.toBeUpdated.match(re) === null) {
-    console.log("Please add http:// to your URL");
+  const currCookie = req.cookies.user_id;
+  if (currCookie === urlDatabase[req.params.id].id) {
+    if (req.body.toBeUpdated.match(re) === null) {
+      console.log("Please add http:// to your URL"); // can use alert() if you wanted to let user know
+    }
+    else {
+      urlDatabase[req.params.id].long = req.body.toBeUpdated;
+    }
+    res.redirect("/urls");
   }
   else {
-    urlDatabase[req.params.id] = req.body.toBeUpdated;
+    res.status(403).send("403: This link does not belong to you ಠ_ಠ");
   }
-  res.redirect("/urls");
 })
 
 app.post("/login", (req, res) => {
-  // console.log(req.body.email, req.body.password);
   for (eachUser in users) {
-    // console.log(users[eachUser].email, users[eachUser].password);
     if (users[eachUser].email === req.body.email && users[eachUser].password === req.body.password) {
-      // console.log("check");
       app.locals.user = req.body.email;
-      // console.log(app.locals.user);
       res.cookie("user_id", users[eachUser].id);
       res.redirect("/");
       return;
     }
   }
-  res.status(403).send("Invalid email or password");
+  res.status(403).send("403: Invalid email or password ಠ_ಠ");
 })
 
 app.get("/login", (req, res) => {
@@ -106,29 +124,25 @@ app.get("/login", (req, res) => {
 })
 
 app.post("/logout", (req, res) => {
-  res.clearCookie(users[app.locals.user]);
+  res.clearCookie('user_id');
   app.locals.user = undefined;
   res.redirect("/urls");
 })
 
 app.get("/register", (req, res) => {
-  // let templateVars = {
-  //   user: users
-  // };
   res.render("registration");
 })
 
 app.post("/register", (req, res) => {
   let randomID = generateRandomString();
   if (req.body === undefined || req.body.password === undefined || req.body.email === undefined) {
-    res.status(400).send("400: Invalid email or password!");
+    res.status(400).send("400: Invalid email or password ¯\_(ツ)_/¯");
   }
   else {
     users[randomID] = {};
     users[randomID].id = randomID;
     users[randomID].email = req.body.email;
     users[randomID].password = req.body.password;
-    // console.log(users);
     app.locals.user = req.body.email;
     res.cookie("user_id", randomID);
     res.redirect("/urls");
@@ -142,4 +156,16 @@ app.listen(PORT, () => {
 function generateRandomString() {
   let random = (Math.random()*2).toString(36);
   return random.slice(2, 8);
+}
+
+function urlsForUser(id) {
+  let filteredObj = {};
+  for (each in urlDatabase) {
+    if (id === urlDatabase[each].id) {
+      filteredObj[each] = {};
+      filteredObj[each].id = id;
+      filteredObj[each].long = urlDatabase[each].long;
+    }
+  }
+  return filteredObj;
 }
