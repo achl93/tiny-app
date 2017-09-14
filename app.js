80baf6418c86c022a2fc06fb0cad237f16f7f08f
Andy Leung <andy_ch_009@hotmail.com>
2017-09-14 18:46:28 +0000

    adding hit counter

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080;
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
 const cookieSession = require("cookie-session");
 const bodyParser = require("body-parser");
 const bcrypt = require('bcrypt');
+const methodOverride = require('method-override');
+
+app.use(methodOverride('_method'));
 
 app.use(cookieSession({
   name: 'session',
@@ -16,11 +19,13 @@ app.set("view engine", "ejs");
 var urlDatabase = {
   "b2xVn2": {
     long: "http://www.lighthouselabs.ca",
-    id: "userRandomID"
+    id: "userRandomID",
+    visitCount: 0
   },
   "9sm5xK": {
     long: "http://www.google.com",
-    id: "user2RandomID"
+    id: "user2RandomID",
+    visitCount: 0
   }
 }
 
@@ -38,7 +43,11 @@ var users = {
 }
 
 app.get("/", (req, res) => {
-  res.end("Hello!");
+  res.format({
+    'text/html': () => {
+      res.send('<p>Hello! Here are your <a href="http://localhost:8080/urls">urls</a>.</p>');
+    }
+  })
 });
 
 app.get("/urls", (req, res) => {
@@ -55,6 +64,7 @@ app.post("/urls", (req, res) => {
   urlDatabase[randomString] = {};
   urlDatabase[randomString].long = req.body.longURL;
   urlDatabase[randomString].id = currCookie;
+  urlDatabase[randomString].visitCount = 0;
   res.redirect('http://localhost:8080/urls/'+randomString);
 });
 
@@ -69,6 +79,7 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL].long;
+  urlDatabase[req.params.shortURL].visitCount++;
   res.redirect(longURL);
 });
 
@@ -76,7 +87,8 @@ app.get("/urls/:id", (req, res) => {
   const currCookie = req.session.user_id;
   if (currCookie === urlDatabase[req.params.id].id) {
     let templateVars = { 
-      shortURL: req.params.id
+      shortURL: req.params.id,
+      visits: urlDatabase[req.params.id].visitCount
     };
     res.render("urls_show", templateVars);
   }
@@ -113,9 +125,8 @@ app.post("/urls/:id/update", (req, res) => {
   }
 })
 
-app.post("/login", (req, res) => {
+app.put("/login", (req, res) => {
   for (eachUser in users) {
-    console.log(users[eachUser].password);
     if (users[eachUser].email === req.body.email && bcrypt.compareSync(req.body.password, users[eachUser].password)) {
       app.locals.user = req.body.email;
       req.session.user_id = users[eachUser].id;
@@ -140,7 +151,7 @@ app.get("/register", (req, res) => {
   res.render("registration");
 })
 
-app.post("/register", (req, res) => {
+app.put("/register", (req, res) => {
   let randomID = generateRandomString();
   if (req.body === undefined || req.body.password === undefined || req.body.email === undefined) {
     res.status(400).send("400: Invalid email or password ¯\_(ツ)_/¯");
@@ -160,7 +171,7 @@ app.post("/register", (req, res) => {
 })
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyApp listening on port ${PORT}!`);
 });
 
 // Helper functions
